a47fb2e0d16dd2f95ef2992be129237b29386be3
won-spec <jswon1110@gmail.com>
2019-12-11 19:36:30 +0900

    네이버 실시간 검색어 수정

--- chatbot/05_naver_rank.py
+++ chatbot/05_naver_rank.py
@@ -1,21 +1,20 @@
 import requests
-from datetime
+# from datetime import datetime
 from bs4 import BeautifulSoup
 
 url = 'https://www.naver.com/'
 html = requests.get(url).text
 soup = BeautifulSoup(html, 'html.parser')
 
-# target = '#PM_ID_ct > div.header > div.section_navbar > div.area_hotkeyword.PM_CL_realtimeKeyword_base > div.ah_roll.PM_CL_realtimeKeyword_rolling_base > div > ul'
-# rank = soup.select_one(target).text
-# print(rank)
-
-target = '#PM_ID_ct > div.header > div.section_navbar > div.area_hotkeyword.PM_CL_realtimeKeyword_base > div.ah_roll.PM_CL_realtimeKeyword_rolling_base > div > ul > li > a > span.ah_k'
-names = soup.select(target).text
-
-
-print(f'{datetime.now()} 기준 실시간 검색어')
-for name in names:
-    print(name.text)
-
+target = '#PM_ID_ct > div.header > div.section_navbar > div.area_hotkeyword.PM_CL_realtimeKeyword_base > div.ah_roll.PM_CL_realtimeKeyword_rolling_base > div > ul'
+rank = soup.select_one(target).text
 print(rank)
+
+# target = '#PM_ID_ct > div.header > div.section_navbar > div.area_hotkeyword.PM_CL_realtimeKeyword_base > div.ah_roll.PM_CL_realtimeKeyword_rolling_base > div > ul > li > a > span.ah_k'
+# names = soup.select(target).text
+
+# print(f'{datetime.now()} 기준 실시간 검색어')
+# for name in names:
+#     print(name.text)
+
+# print(rank)
